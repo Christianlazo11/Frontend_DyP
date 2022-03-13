5927ff1b30fb445a252c937a2a3a34c080bc3e6c
christian lazo <kenishi.cristianxd@gmail.com>
2022-03-13 08:12:32 -0500

    fix bug with never used

--- src/components/footer/SocialMediaItems.js
+++ src/components/footer/SocialMediaItems.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa";
-import { Link } from "react-router-dom";
 
 const SocialMediaItems = () => {
   return (
